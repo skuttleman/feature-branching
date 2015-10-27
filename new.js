eed157eba55d481d5025b2b760260f53e6f10daf
Ben Allred <skuttleman@gmail.com>
2015-10-27 17:52:18 -0600

    edited new.js

--- new.js
+++ new.js
@@ -1,5 +1,5 @@
 Number.prototype.factorial = function() {
   if (this < 1) throw 'Invalid Input';
   if (this <= 2) return Number(this);
-  else return this * (this - 1).factorial();
+  else { return this * (this - 1).factorial(); }
 }
