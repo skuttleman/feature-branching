44a0f6a8c97ad1487d909577841e12a7ec07f2ba
Ben Allred <skuttleman@gmail.com>
2015-10-27 16:48:37 -0600

    Fixed syntax error

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ function MyObject() {};
 
 MyObject.prototype.isPrime = function(x) {
   for (var i = 2; i < Math.sqrt(x); i ++)
-    if (x % i ==== 0) return false;
+    if (x % i === 0) return false;
   return true;
 };
